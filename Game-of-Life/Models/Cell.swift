e9047b766d46dad60c3758c5a48f3857d3c11453
Gi Pyo Kim <gipyok@gmail.com>
2020-07-27 10:08:02 -0700

    Created Cell model

---
+++ Game-of-Life/Models/Cell.swift
@@ -0,0 +1,13 @@
+//
+//  Cell.swift
+//  Game-of-Life
+//
+//  Created by Gi Pyo Kim on 7/27/20.
+//  Copyright © 2020 gipgip. All rights reserved.
+//
+
+import Foundation
+
+struct Cell {
+    let alive: Bool
+}
